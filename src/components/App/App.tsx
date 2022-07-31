99e56770ae6be86aec4f3efc36b34ba67b2d3264
Henry Miller <mail.henrym+git@gmail.com>
2022-07-30 20:03:31 -0400

    add fib function

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  function fibonacci(num: number): any{
+    if (num <= 2) return 1
+    return fibonacci(num-1) + fibonacci(num-2)
+  }
+
   return (
-    <div></div>
+    <div>{fibonacci(6)}</div>
   );
 }
 
