d9ff746553e7b9a7225e29b5633d93252a008390
Henry Miller <mail.henrym+git@gmail.com>
2022-07-31 18:04:04 -0400

    refactor into Input component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useRef } from 'react';
+import Input from '../Input/Input';
 import './App.css';
 
 function App() {
   let [input, setInput] = useState(0)
 
-  // let [ending,setEnding] = useState('th')
-  let ending = 'th'
 
   const numInputRef = useRef<HTMLInputElement>(null);
 
@@ -13,56 +12,17 @@ function App() {
     setInput(Number(numInputRef.current!.value))
   }
 
-  let stringInput = input.toString().split('')
-  let lastDigit = Number(stringInput[stringInput.length-1])
-  
-  switch(lastDigit){
-    case 3:
-      ending = 'rd'
-      break
-    case 2:
-      ending = 'nd'
-      break
-    case 1:
-      ending = 'st'
-      break
-    default:
-      ending = 'th'
-      break
-  }
-  
-  function fibonacci(num: number): any {
-    if (num <= 2) return 1
-    return fibonacci(num - 1) + fibonacci(num - 2)
-  }
-
-  function fib(num:number, memo :{ [key:string]: any} = {}): any { // solve answer once and store in memo
-    if (memo[num] !== undefined) return memo[num]
-    if (num <= 2) return 1
-    let res = fib(num - 1, memo) + fib(num - 2, memo)
-    memo[num] = res
-    return res
-}
-  console.log(typeof(fib(6)))
 
   return (
-    <div className='main-container'>
-      <input
-        className='num-input'
-        type='number'
-        min='0'
-        max='99'
-        step='1'
-        value={input ? input : ''}
-        placeholder='0'
-        ref={numInputRef}
-        onChange={handleInputChange}
-      />
-      <div className='input-ending'>{ending}</div>
-      <div>:</div>
-      <div>{fib(input)}</div>
-    </div>
+    <>
+    <Input
+      input={input}
+      handleInputChange={handleInputChange}
+      numInputRef={numInputRef}
+    />
+    </>
   );
+
 }
 
 export default App;
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+
+
+
+const Input: React.FC<{input:number, handleInputChange: any, numInputRef: any}> = ({input, handleInputChange, numInputRef}) => {
+
+    let ending = 'th'
+
+
+    let stringInput = input.toString().split('')
+    let lastDigit = Number(stringInput[stringInput.length - 1])
+
+    switch (lastDigit) {
+        case 3:
+            ending = 'rd'
+            break
+        case 2:
+            ending = 'nd'
+            break
+        case 1:
+            ending = 'st'
+            break
+        default:
+            ending = 'th'
+            break
+    }
+
+
+  function fibonacci(num: number): any {
+    if (num <= 2) return 1
+    return fibonacci(num - 1) + fibonacci(num - 2)
+  }
+
+  function fib(num:number, memo :{ [key:string]: any} = {}): any { // solve answer once and store in memo
+    if (memo[num] !== undefined) return memo[num]
+    if (num <= 2) return 1
+    let res = fib(num - 1, memo) + fib(num - 2, memo)
+    memo[num] = res
+    return res
+}
+  console.log(typeof(fib(6)))
+
+    return <div className='input-container'>
+        <input
+            className='num-input'
+            type='number'
+            min='0'
+            max='99'
+            step='1'
+            value={input ? input : ''}
+            placeholder='0'
+            ref={numInputRef}
+            onChange={handleInputChange} />
+        <div className='input-ending'>{ending}</div>
+        <div>:</div>
+        <div>{fib(input)}</div>
+    </div>;
+}
+
+export default Input
