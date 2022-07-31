92115c0ad302466131dadccbcf8ccf885553356d
Henry Miller <mail.henrym+git@gmail.com>
2022-07-30 20:17:18 -0400

    handling input change

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef} from 'react';
 import './App.css';
 
 function App() {
   let [result,setResult] = useState(0)
+  
+  const numInputRef = useRef<HTMLInputElement>(null);
+
+  function handleResultChange(){
+    setResult(Number(numInputRef.current!.value))
+  }
+
 
   function fibonacci(num: number): any{
     if (num <= 2) return 1
@@ -17,8 +24,11 @@ function App() {
       step='1'
       value={result ? result : ''}
       placeholder='Enter a Number'
+      ref={numInputRef}
+      onChange={handleResultChange}
     />
     <div>{fibonacci(6)}</div>
+    <div>{result}</div>
     </>
   );
 }
