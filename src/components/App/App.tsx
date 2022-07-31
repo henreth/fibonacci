113b6a989d38d979e36b58ff71d27da15c3d35c6
Henry Miller <mail.henrym+git@gmail.com>
2022-07-31 17:06:25 -0400

    add input ending

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,9 @@ import './App.css';
 function App() {
   let [result, setResult] = useState(0)
 
+  // let [ending,setEnding] = useState('th')
+  let ending = 'th'
+
   const numInputRef = useRef<HTMLInputElement>(null);
 
   function handleResultChange() {
@@ -16,26 +19,46 @@ function App() {
     return fibonacci(num - 1) + fibonacci(num - 2)
   }
 
-  function fib(num: number, memo = [undefined, 1, 1]): any {
+  function fib(num:number, memo :{ [key:string]: any} = {}): any { // solve answer once and store in memo
     if (memo[num] !== undefined) return memo[num]
+    if (num <= 2) return 1
     let res = fib(num - 1, memo) + fib(num - 2, memo)
     memo[num] = res
     return res
+}
+  console.log(typeof(fib(6)))
+  
+  switch(result){
+    case 3:
+      ending = 'rd'
+      break
+    case 2:
+      ending = 'nd'
+      break
+    case 1:
+      ending = 'st'
+      break
+    default:
+      ending = 'th'
+      break
   }
-
 
   return (
     <div className='main-container'>
       <input
+        className='num-input'
         type='number'
         min='0'
+        max='100'
         step='1'
         value={result ? result : ''}
-        placeholder='Enter a Number'
+        placeholder='Enter'
         ref={numInputRef}
         onChange={handleResultChange}
       />
-      <div>{fibonacci(result)}</div>
+      <div>{ending}</div>
+      <div>:</div>
+      <div>{fib(result)}</div>
     </div>
   );
 }
